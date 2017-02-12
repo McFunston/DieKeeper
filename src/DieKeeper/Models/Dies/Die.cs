a477e0ea1326087f3f187b28c311da463c741024
Mica Funston <Mica Funston@gmail.com>
2017-02-12 18:33:31 -0500

    Initial. Update NuGet packages (and deal with consequences). Begin adding models.

---
+++ src/DieKeeper/Models/Dies/Die.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DieKeeper.Models
+{
+    public enum DieType
+    {
+        PocketFolder,
+        Score,
+        Perf,
+        Box,
+        Sleeve,
+        Other
+    }
+
+    public abstract class Die
+    {
+        public int ID { get; set; }
+        public int BaseWidth { get; set; }
+        public int BaseHeight { get; set; }
+        public string Description { get; set; }
+        public DieType Type { get; set; }
+        public Image FullDieImage { get; set; }
+        public Image AnnotatedDieImage { get; set; }
+        public Image SampleImage { get; set; }
+        //public virtual List<T> Attributes { get; set; }
+    }
+}
